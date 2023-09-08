385939c31df35bffe62841ee6ffb31a83aa08430
Nadia <nadiawellela@gmail.com>
2023-09-08 00:32:59 -0700

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,9 +11,10 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
-	func(tree->n);
-	binary_tree_preorder(tree->right, func);
-	binary_tree_preorder(tree->left, func);
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
