709082ef3a2c24ea46fc35b6efb0e5d485adf88b
Nadia <nadiawellela@gmail.com>
2023-09-08 05:57:21 -0700

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -26,6 +26,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node || node->parent)
-		return (binary_tree_sibling(node->parent));
+		if (binary_tree_sibling(node->parent))
+			return (binary_tree_sibling(node->parent));
 	return (NULL);
 }
