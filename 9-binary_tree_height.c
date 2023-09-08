e22ee0376fa7cb6f5c1ab2f3883178be0953353a
Nadia <nadiawellela@gmail.com>
2023-09-08 01:52:10 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
-{	
+{
 	size_t left = 0, right = 0;
 
 	if (tree)
@@ -19,8 +19,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		if (tree->right)
 			right += binary_tree_height(tree->right) + 1;
 	}
-	if(left > right)
-		return left;
-	else 
-		return right;
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
